995cb956eecffbd7648e890d944123ed611b0807
pierre ronde <o0o@E4310.home>
2018-03-16 09:27:34 +0100

    couleurs pour la profondeur, on voit tout les éléments du cube;

--- main.cpp
+++ main.cpp
@@ -5,11 +5,25 @@
 
 int main(int argc, char *argv[]){
     initscr();cbreak();
+    if(has_colors() == false) mvprintw(0, 10, "Terminal doesn't support colors..\n");
+    start_color();
+    init_pair(1, COLOR_RED, COLOR_WHITE);
+    init_pair(2, COLOR_CYAN, COLOR_BLACK);
+    init_pair(3, COLOR_MAGENTA, COLOR_WHITE);
+    init_pair(4, COLOR_YELLOW, COLOR_BLACK);
+    init_pair(5, COLOR_GREEN, COLOR_WHITE);
+    init_pair(6, COLOR_BLUE, COLOR_BLACK);
+    init_pair(7, COLOR_CYAN, COLOR_WHITE);
+    init_pair(8, COLOR_RED, COLOR_BLACK);
+    
+    
 	int row, col;
     getmaxyx(stdscr, row, col);
     printw("rows:%d\ncol:%d", row, col);
     refresh();
-    int N = 4;//on récupère le premier argument du programme.
+    
+    
+    int N = 8;//on récupère le premier argument du programme.
 	int spins[N][N][N];
     double value;
     char A[20];
@@ -25,11 +39,14 @@ int main(int argc, char *argv[]){
                                             & ((k & (int)pow(2.0, (double)y)) >> y) \
                                             & ((l & (int)pow(2.0, (double)z)) >> z);
                             sprintf(A,"%d", spins[x][y][z]);
-                            mvprintw((y*2+row/2-z)+3, (x*2+z)+3, A);
+                            attron(COLOR_PAIR(z+1));
+                            mvprintw((y*3+row/2-z)+3, (x*4+z)+3, A);
+                            attroff(COLOR_PAIR(z+1));/*
                             if(y != 0)
-                                mvprintw((y*2+row/2-z)+2, (x*2+z)+3, "|");
-                            mvprintw((y*2+row/2-z)+3, (x*2+z)+2, "-");
-                            
+                                mvprintw((y*3+row/2-z)+2, (x*4+z)+3, "|");
+                            if(x != 0 && x != N - 1)
+                                mvprintw((y*3+row/2-z)+3, (x*4+z)+2, "-");
+                            */
                         }
                     }
                 }
